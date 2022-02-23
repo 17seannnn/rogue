a2d4e93033a20959a0948dc452e719de25775942
17seannnn <17sean@protonmail.com>
2022-02-23 16:03:48 +0300

    build: update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-gcc -ansi -pedantic -Wall -g -Og -lncurses -DDEBUG main.c -o rogue
+gcc -ansi -pedantic -Wall -g -Og -lncurses -DDEBUG -c math.c curses.c room.c path.c level.c fov.c cmd.c hunter.c
+gcc -ansi -pedantic -Wall -g -Og -lncurses -DDEBUG math.c curses.o room.o path.o level.o fov.o cmd.o hunter.o main.c -o rogue
