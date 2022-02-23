c6b7ab2b43eb0e29886e42125c0f1c25b3b1608c
17seannnn <17sean@protonmail.com>
2022-02-23 16:49:01 +0300

    build: upgrade build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-gcc -ansi -pedantic -Wall -g -Og -lncurses -DDEBUG -c math.c curses.c room.c path.c level.c fov.c cmd.c hunter.c
-gcc -ansi -pedantic -Wall -g -Og -lncurses -DDEBUG math.c curses.o room.o path.o level.o fov.o cmd.o hunter.o main.c -o rogue
+NAME=rogue
+CC=gcc
+
+case $1 in
+        install)
+        gcc -ansi -pedantic -Wall -g -Og -lncurses -DDEBUG -c math.c curses.c room.c path.c level.c fov.c cmd.c hunter.c
+        gcc -ansi -pedantic -Wall -g -Og -lncurses -DDEBUG math.c curses.o room.o path.o level.o fov.o cmd.o hunter.o main.c -o rogue;;
+        clean)
+                rm *.o $NAME log;;
+        * | --help) echo "\
+Usage:
+build.sh [COMMAND/--OPTION]
+
+Commands:
+        install    install this package
+        clean      clean src
+        help       show help";;
+esac
