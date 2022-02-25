126d5cd41203d110acbf67a7579b576a49e01907
17seannnn <17sean@protonmail.com>
2022-02-25 17:55:52 +0300

    refactor: move func

--- src/cmd.c
+++ src/cmd.c
@@ -2,62 +2,48 @@
 
 #include "rogue.h"
 
-static void move_hunter(int c, struct hunter *h, struct level *l)
+static void do_move(struct hunter *h, int dx, int dy)
 {
-        switch (c) {
-        case 'h':
-                if (can_move(l, h->pos.x-1, h->pos.y))
-                        h->pos.x -= 1;
-                break;
-        case 'j':
-                if (can_move(l, h->pos.x, h->pos.y+1))
-                        h->pos.y += 1;
-                break;
-        case 'k':
-                if (can_move(l, h->pos.x, h->pos.y-1))
-                        h->pos.y -= 1;
-                break;
-        case 'l':
-                if (can_move(l, h->pos.x+1, h->pos.y))
-                        h->pos.x += 1;
-                break;
-        case 'y':
-                if (can_move(l, h->pos.x-1, h->pos.y-1)) {
-                        h->pos.x -= 1;
-                        h->pos.y -= 1;
-                }
-                break;
-        case 'u':
-                if (can_move(l, h->pos.x+1, h->pos.y-1)) {
-                        h->pos.x += 1;
-                        h->pos.y -= 1;
-                }
-                break;
-        case 'b':
-                if (can_move(l, h->pos.x-1, h->pos.y+1)) {
-                        h->pos.x -= 1;
-                        h->pos.y += 1;
-                }
-                break;
-        case 'n':
-                if (can_move(l, h->pos.x+1, h->pos.y+1)) {
-                        h->pos.x += 1;
-                        h->pos.y += 1;
-                }
-                break;
-        default:
-                break;
-        }
+        h->pos.x += dx;
+        h->pos.y += dy;
 }
 
 void do_cmd(int c, struct hunter *h, struct level *l)
 {
-                switch (c) {
-                case 'h': case 'j': case 'k': case 'l':
-                case 'y': case 'u': case 'b': case 'n':
-                        move_hunter(c, h, l);
-                        break;
-                default:
-                        break;
-                }
+        switch (c) {
+        case 'h':
+                if (can_move(l, h->pos.x-1, h->pos.y))
+                        do_move(h, -1, 0);
+                break;
+        case 'j':
+                if (can_move(l, h->pos.x, h->pos.y+1))
+                        do_move(h, 0, 1);
+                break;
+        case 'k':
+                if (can_move(l, h->pos.x, h->pos.y-1))
+                        do_move(h, 0, -1);
+                break;
+        case 'l':
+                if (can_move(l, h->pos.x+1, h->pos.y))
+                        do_move(h, 1, 0);
+                break;
+        case 'y':
+                if (can_move(l, h->pos.x-1, h->pos.y-1))
+                        do_move(h, -1, -1);
+                break;
+        case 'u':
+                if (can_move(l, h->pos.x+1, h->pos.y-1))
+                        do_move(h, 1, -1);
+                break;
+        case 'b':
+                if (can_move(l, h->pos.x-1, h->pos.y+1))
+                        do_move(h, -1, 1);
+                break;
+        case 'n':
+                if (can_move(l, h->pos.x+1, h->pos.y+1))
+                        do_move(h, 1, 1);
+                break;
+        default:
+                break;
+        }
 }
