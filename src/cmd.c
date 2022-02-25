291d2b4b44ec3062d9115c663f4d470e6e1722fc
17seannnn <17sean@protonmail.com>
2022-02-25 16:27:27 +0300

    feat: add additional moves yubn

--- src/cmd.c
+++ src/cmd.c
@@ -21,6 +21,30 @@ static void move_hunter(int c, struct hunter *h, struct level *l)
                 if (can_move(l, h->pos.x+1, h->pos.y))
                         h->pos.x += 1;
                 break;
+        case 'y':
+                if (can_move(l, h->pos.x-1, h->pos.y-1)) {
+                        h->pos.x -= 1;
+                        h->pos.y -= 1;
+                }
+                break;
+        case 'u':
+                if (can_move(l, h->pos.x+1, h->pos.y-1)) {
+                        h->pos.x += 1;
+                        h->pos.y -= 1;
+                }
+                break;
+        case 'b':
+                if (can_move(l, h->pos.x-1, h->pos.y+1)) {
+                        h->pos.x -= 1;
+                        h->pos.y += 1;
+                }
+                break;
+        case 'n':
+                if (can_move(l, h->pos.x+1, h->pos.y+1)) {
+                        h->pos.x += 1;
+                        h->pos.y += 1;
+                }
+                break;
         default:
                 break;
         }
@@ -30,6 +54,7 @@ void do_cmd(int c, struct hunter *h, struct level *l)
 {
                 switch (c) {
                 case 'h': case 'j': case 'k': case 'l':
+                case 'y': case 'u': case 'b': case 'n':
                         move_hunter(c, h, l);
                         break;
                 default:
