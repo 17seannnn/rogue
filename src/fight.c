56cdccfcd2966b9a8692639a40a494257c6384c9
17seannnn <17sean@protonmail.com>
2022-03-04 22:13:40 +0300

    refactor: remove long hit message

--- src/fight.c
+++ src/fight.c
@@ -7,7 +7,6 @@
 static const char *msg_hit[] = {
         " hit ",
         " have injured ",
-        " scored an excellent hit on ",
         " swing and hit "
 };
 
@@ -16,11 +15,11 @@ int attack(const struct creature *a, struct creature *d)
         d->hp -= a->dmg;
         if (a->cast == cast_hunter) {
                 add_msg("You");
-                append_msg(msg_hit[rand() % 4]);
+                append_msg(msg_hit[rand() % 3]);
                 append_msg("the beast.");
         } else {
                 add_msg("The beast");
-                append_msg(msg_hit[rand() % 4]);
+                append_msg(msg_hit[rand() % 3]);
                 append_msg("you.");
         }
         return 1;
