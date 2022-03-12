d24c46dbc101efcb178cc62d54d06c4a0adca064
17seannnn <17sean@protonmail.com>
2022-03-13 01:20:32 +0300

    feat: upgrade inventory show

--- src/inv.c
+++ src/inv.c
@@ -7,22 +7,37 @@ void show_inv(struct loot_list *inv)
         int y = 0, x = 0;
         struct loot_list *t;
         wclear(invw);
+        wattrset(invw, A_REVERSE);
         mvwprintw(invw, y, x, "Weapons");
-        for (t = inv; t; t = t->next, y++)
-                if (t->l->type == type_weapon)
+        wattrset(invw, A_NORMAL);
+        for (y++, t = inv; t; t = t->next) {
+                if (t->l->type == type_weapon) {
                         mvwprintw(invw, y, x, t->l->name);
-        y++;
+                        y++;
+                }
+        }
+        wattrset(invw, A_REVERSE);
         mvwprintw(invw, y, x, "Armor");
-        for (t = inv; t; t = t->next, y++)
-                if (t->l->type == type_armor)
+        wattrset(invw, A_NORMAL);
+        for (y++, t = inv; t; t = t->next) {
+                if (t->l->type == type_armor) {
                         mvwprintw(invw, y, x, t->l->name);
-        y++;
+                        y++;
+                }
+        }
+        wattrset(invw, A_REVERSE);
         mvwprintw(invw, y, x, "Poitions");
-        for (t = inv; t; t = t->next, y++)
-                if (t->l->type == type_poition)
+        wattrset(invw, A_NORMAL);
+        for (y++, t = inv; t; t = t->next) {
+                if (t->l->type == type_poition) {
                         mvwprintw(invw, y, x, t->l->name);
+                        y++;
+                }
+        }
         y++;
+        wattrset(invw, A_REVERSE);
         mvwprintw(invw, y, x, "(end)");
+        wattrset(invw, A_NORMAL);
         wrefresh(invw);
         wait_ch(' ');
 }
