13d9b50c634b90964148684b03572ae162cd5f98
17seannnn <17sean@protonmail.com>
2022-03-14 20:41:21 +0300

    feat: msg about killing creature

--- src/fight.c
+++ src/fight.c
@@ -10,19 +10,15 @@ static const char *msg_hit[] = {
         " swing and hit "
 };
 
+static const char msg_kill[] = " killed ";
+
 int attack(const struct creature *a, struct creature *d)
 {
         int dmg;
         dmg = a->weapon ? a->weapon->dmg + a->dmg : a->dmg;
         d->hp -= dmg;
-        if (a->cast == cast_hunter) {
-                add_msg("You");
-                append_msg(msg_hit[rand() % 3]);
-                append_msg("the beast.");
-        } else {
-                add_msg("The beast");
-                append_msg(msg_hit[rand() % 3]);
-                append_msg("you.");
-        }
+        add_msg(a->cast == cast_hunter ? "You" : "The beast");
+        append_msg(d->hp > 0 ? msg_hit[rand() % 3] : msg_kill);
+        append_msg(d->cast == cast_hunter ? "you." : "the beast.");
         return 1;
 }
