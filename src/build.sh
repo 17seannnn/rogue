086c1bdbfcac504a1f02f6c2cdd0fbaf64f8c788
17seannnn <17sean@protonmail.com>
2022-03-22 18:40:03 +0300

    build: remove optimization to remove errors from valgrind

--- src/build.sh
+++ src/build.sh
@@ -5,8 +5,8 @@ CC=gcc
 
 case $1 in
         install)
-        gcc -ansi -pedantic -Wall -Wextra -g -Og -lncurses -DDEBUG -c math.c curses.c room.c path.c loot.c inv.c creature.c hunter.c beast.c level.c msg.c fov.c fight.c cmd.c
-        gcc -ansi -pedantic -Wall -Wextra -g -Og -lncurses -DDEBUG math.c curses.o room.o path.o loot.o inv.o creature.c hunter.o beast.o level.o msg.o fov.o fight.o cmd.o main.c -o rogue;;
+        gcc -ansi -pedantic -Wall -Wextra -g -lncurses -DDEBUG -c math.c curses.c room.c path.c loot.c inv.c creature.c hunter.c beast.c level.c msg.c fov.c fight.c cmd.c
+        gcc -ansi -pedantic -Wall -Wextra -g -lncurses -DDEBUG math.c curses.o room.o path.o loot.o inv.o creature.c hunter.o beast.o level.o msg.o fov.o fight.o cmd.o main.c -o rogue;;
         clean)
                 rm *.o $NAME log;;
         * | --help) echo "\
