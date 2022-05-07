a20335a2e32bcdc308e4d10c593f9054816f7468
17seannnn <17sean@protonmail.com>
2022-05-07 15:22:41 +0300

    feat: add misses

--- src/fight.c
+++ src/fight.c
@@ -5,10 +5,20 @@
 
 #include "rogue.h"
 
+enum {
+	msg_hit_count  = 3,
+	msg_miss_count = 2,
+	miss_chance    = 5
+};
+
 static const char *msg_hit[] = {
         " hit ",
         " have injured ",
         " swing and hit "
+};
+static const char *msg_miss[] = {
+	" miss ",
+	" don't hit "
 };
 static const char msg_kill[] = " killed ";
 static const char msg_max_blood[] = "You can't carry more blood.";
@@ -17,18 +27,22 @@ int attack(struct level *l, struct creature *a, struct creature *d)
 {
         int dmg, res;
 	char buf[bufsize];
-        dmg = a->weapon ? a->weapon->l.val + a->dmg : a->dmg;
-        d->hp -= dmg;
         add_msg(a->cast == cast_hunter ? "You" : a->name);
-        if (d->hp <= 0) {
-		if (a->cast == cast_hunter) {
-			res = add_blood(l, a, d->blood);
-			add_exp(a, d->exp);
+	if (rand() % 100 >= miss_chance) {
+		dmg = a->weapon ? a->weapon->l.val + a->dmg : a->dmg;
+		d->hp -= dmg;
+		if (d->hp <= 0) {
+			if (a->cast == cast_hunter) {
+				res = add_blood(l, a, d->blood);
+				add_exp(a, d->exp);
+			}
+			append_msg(msg_kill);
+		} else {
+			append_msg(msg_hit[rand() % msg_hit_count]);
 		}
-                append_msg(msg_kill);
-        } else {
-                append_msg(msg_hit[rand() % 3]);
-        }
+	} else {
+		append_msg(msg_miss[rand() % msg_miss_count]);
+	}
 	if (d->cast == cast_hunter) {
 		append_msg("you.");
 	} else {
